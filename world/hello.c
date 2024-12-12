25e5b34e23025815b83ba62f259cc2ed0e9464c0
HassanZakie75 <hassanzakieyusoff@gmail.com>
2024-12-12 15:12:13 +0800

    Add files via upload

---
+++ world/hello.c
@@ -0,0 +1,10 @@
+// Header file for input output functions
+#include <stdio.h>
+
+// Main function: entry point for execution
+int main()
+{
+    // printf() displays the string inside quotation
+    printf("hello, world \n");
+    return 0;
+}
